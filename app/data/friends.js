a4096c485e8a6ccbbea8e3543208d4f110ea7cfe
Greatest Dane <DaneGoodman85@gmail.com>
2019-11-08 22:56:22 -0800

    Do you ever lay awake at night and just wonder what it is, exactly, that you do here on earth?

---
+++ app/data/friends.js
@@ -0,0 +1,109 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj88p3vwNzlAhWHrZ4KHV5pCuUQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F718113103059412741%2F&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jacob Deming",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6w4mswdzlAhWMqp4KHe2kAzoQjRx6BAgBEAQ&url=https%3A%2F%2Ffreehindistatus.com%2Fall-about-whatsapp-profile-images-for-you-guys%2F&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Jeremiah Scanlon",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiD8vO9wdzlAhXYi54KHT0CDPoQjRx6BAgBEAQ&url=https%3A%2F%2Ftwistedsifter.com%2F2012%2F09%2Fone-trippy-profile-pic%2F&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Louis T. Delia",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiE0p3FwdzlAhUCnZ4KHXkQCTMQjRx6BAgBEAQ&url=https%3A%2F%2Fhostsandhomes.com%2Fairbnb-remove-profile-pictures%2F&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Lou Ritter",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi4ntHTwdzlAhUJt54KHVbBCoUQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.independent.ie%2Fentertainment%2Fbanter%2Fyou-probably-shouldnt-show-your-kids-peppa-pigs-terrifying-front-profile-36808545.html&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Jordan Biason",
+      photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwii17vkwdzlAhWGuZ4KHUl1C9sQjRx6BAgBEAQ&url=https%3A%2F%2Fmedium.com%2Finside-blogging%2Fthe-worst-mistakes-in-choosing-your-profile-picture-1c8a8ad6bee8&psig=AOvVaw29FM0lhBak3O7CjRr1w9x5&ust=1573367393917991",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
